fdb21728ae64a78fb35e030ddedd53702f29e1a3
Alejandro-Gonzale-z <greeny090315@gmail.com>
2025-04-02 14:51:28 -0400

    Added swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,15 @@ async function bootstrap() {
     origin: `${process.env.SB_URL}`,
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Schedule Builder')
+    .setDescription('The Schedule Builder API description')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 4000);
 }
