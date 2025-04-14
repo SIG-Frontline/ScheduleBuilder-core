7ee0feeda7a0f521cde7e1d5032d3d9448fb13d8
Anthony M <38088724+Creeperman1524@users.noreply.github.com>
2025-04-14 13:24:10 -0400

    Recommender and organizer Swagger documentation
    Added documentation from SwaggerUI to the recommender and organizer endpoints

--- src/organizer/organizer.controller.ts
+++ src/organizer/organizer.controller.ts
@@ -1,12 +1,29 @@
 import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { OrganizerService } from './organizer.service';
 import { PlanData } from 'src/utils/types.util';
+import { ApiOkResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('')
 export class OrganizerController {
   constructor(private readonly organizerService: OrganizerService) {}
 
   @Post('organizer/')
+  @ApiOkResponse({
+    description: 'Organized plan was returned successfully',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'No plan/courses selected',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'No valid schedules could be made',
+  })
+  @ApiOperation({
+    summary: 'Used to return an optimized plan to a user',
+    description:
+      "Returns an optimized plan based on the input plan and the user's selected filters/options<br>The entire plan must be passed in the body of the request",
+  })
   async organizePlan(@Body() plan: PlanData) {
     if (!plan) throw new BadRequestException('No plan provided');
 
