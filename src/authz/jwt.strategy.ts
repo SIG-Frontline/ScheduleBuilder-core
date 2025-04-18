776ab93ae8ed5a7be6d0f99e77267d9dcbe930f4
Ethan H <ekh3@njit.edu>
2025-04-18 17:44:05 -0400

    fixed type errors

--- src/authz/jwt.strategy.ts
+++ src/authz/jwt.strategy.ts
@@ -1,45 +1,48 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
-import { Logger } from '@nestjs/common';
+import {
+  ExtractJwt,
+  Strategy,
+  SecretOrKeyProvider,
+  StrategyOptionsWithoutRequest,
+} from 'passport-jwt';
 import * as jwksRsa from 'jwks-rsa';
+
+interface UserPayload {
+  iss: string;
+  sub: string;
+  aud: string[];
+  iat: number;
+  exp: number;
+  scope: string;
+  azp: string;
+}
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
-    Logger.debug(process.env.AUTH0_DOMAIN);
-    Logger.debug(process.env.AUTH0_AUDIENCE);
     super({
-      // Dynamically provide signing keys based on the kid in the header and
-      // the JWKS endpoint from your Auth0 tenant
       secretOrKeyProvider: jwksRsa.passportJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
         jwksUri: `${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-      }),
+      }) as SecretOrKeyProvider,
 
-      // where to find the JWT on incoming requests
+      // obtain JWT as bearer token
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 
       // validate these claims
-      audience: process.env.AUTH0_AUDIENCE, // your API identifier
+      audience: process.env.AUTH0_AUDIENCE,
       issuer: `${process.env.AUTH0_DOMAIN}/`,
       algorithms: ['RS256'],
-    } as StrategyOptions);
+      passReqToCallback: false,
+    } as StrategyOptionsWithoutRequest);
   }
 
   // `validate()` is called once the token is verified;
-  // its return value is injected as `req.user`
-  // eslint-disable-next-line @typescript-eslint/require-await
-  async validate(payload: any) {
-    // you can add more checks here (e.g. user exists in your DB)
-    Logger.debug(payload);
-    return { sub: payload.sub, scope: payload.scope, ...payload };
+  validate(payload: UserPayload) {
+    // Any other items needed per validation can be added here
+    return { sub: payload.sub, scope: payload.scope };
   }
 }
--- src/authz/user.decorator.ts
+++ src/authz/user.decorator.ts
@@ -1,18 +1,18 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from '@nestjs/common';
 export interface Auth0User {
   sub: string;
   name: string;
-  email?: string;
-  // add other Auth0 fields as needed
+}
+
+interface UserParam extends Request {
+  user: Auth0User;
 }
 
 export const User = createParamDecorator(
   (data: keyof Auth0User, ctx: ExecutionContext): Auth0User | string => {
-    const request = ctx.switchToHttp().getRequest();
-    const user = request.user as Auth0User;
-    return data ? user[data]! : user;
+    const request: UserParam = ctx.switchToHttp().getRequest();
+    const user = request.user;
+    return data ? user[data] : user;
   },
 );
