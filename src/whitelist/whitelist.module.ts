4102737ae730cfabd56745cfaed5fea9e738c2d9
Ethan H <47959663+WokFriedE@users.noreply.github.com>
2025-05-05 14:21:25 -0400

    Feat whitelist (#22)
    * Added white list items and super admin key
    
    * Updated ENV

---
+++ src/whitelist/whitelist.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserWhitelist, UserWhitelistSchema } from 'schemas/userWhitelist';
+import { WhitelistService } from './whitelist.service';
+import { WhitelistController } from './whitelist.controller';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: UserWhitelist.name, schema: UserWhitelistSchema },
+    ]),
+  ],
+  controllers: [WhitelistController],
+  providers: [WhitelistService],
+})
+export class WhitelistModule {}
